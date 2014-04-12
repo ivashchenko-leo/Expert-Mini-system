9d1a125c092eeb1a1dffb043571e695692339889
Uncrowned <link_92@list.ru>
2014-04-12 15:44:30 +0700

    first commit

---
+++ fileio.h
@@ -0,0 +1,18 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include <QObject>
+
+class FileIO : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FileIO(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // FILEIO_H
